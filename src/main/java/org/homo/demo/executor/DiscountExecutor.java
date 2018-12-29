3c35785f1c37a304b6f7bad36c1c285dae115a58
wujianchuan <wujianchuan@shinow.com>
2018-12-29 22:38:42 +0800

    删除无用变量。

--- src/main/java/org/homo/demo/executor/DiscountExecutor.java
+++ src/main/java/org/homo/demo/executor/DiscountExecutor.java
@@ -7,9 +7,6 @@ import org.homo.controller.HomoRequest;
 import org.homo.demo.service.OrderServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author wujianchuan 2018/12/29
  */
@@ -25,8 +22,6 @@ public class DiscountExecutor implements HomoExecutor {
 
     @Override
     public ExecutionResult execute(HomoRequest request) {
-        Map<String, Object> parameter = new HashMap<>(2);
-        parameter.put("uuid", "01");
         String price = (String) orderService.handle(orderService.discount, request);
         return ExecutionResult.newSuccessInstance("成功", "打折成功", "VIP-" + price);
     }
