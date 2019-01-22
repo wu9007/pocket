bf191e4c9d012dfbd2d6cb3798dffc1b0ef0c2f1
wujianchuan <wujianchuan@shinow.com>
2019-01-22 09:09:43 +0800

    update.

--- src/main/java/org/hunter/Application.java
+++ src/main/java/org/hunter/Application.java
@@ -2,7 +2,6 @@ package org.hunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author wujianchuan 2018/12/25
