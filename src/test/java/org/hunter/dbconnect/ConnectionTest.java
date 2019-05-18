20ecae3913ae3c7e111399272bf305f3c0ab13fd
leyan95 <18231926271@163.com>
2019-05-18 15:18:04 +0800

    update.

---
+++ src/test/java/org/hunter/dbconnect/ConnectionTest.java
@@ -0,0 +1,34 @@
+package org.hunter.dbconnect;
+
+import org.hunter.Application;
+import org.hunter.PocketExecutor;
+import org.hunter.pocket.config.DatabaseConfig;
+import org.hunter.pocket.connect.ConnectionManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.Executors;
+
+/**
+ * @author wujianchuan 2019/1/16
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ConnectionTest {
+    private static final int THREAD_NUM = 100;
+
+    @Autowired
+    DatabaseConfig databaseConfigs;
+
+    @Test
+    public void test1() throws InterruptedException {
+        PocketExecutor.execute(Executors.newFixedThreadPool(THREAD_NUM), THREAD_NUM, () -> {
+            ConnectionManager.getInstance().getConnection(databaseConfigs.getNode().get(0));
+            ConnectionManager.getInstance().getConnection(databaseConfigs.getNode().get(1));
+        });
+        ConnectionManager.getInstance().destroy();
+    }
+}
